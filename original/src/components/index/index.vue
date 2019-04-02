19c49306b19223437a6b3a57f394d5a50a221d9c
dmx123456 <13470003696@163.com>
2019-04-02 20:21:20 +0800

    '修改'

--- original/src/components/index/index.vue
+++ original/src/components/index/index.vue
@@ -9,12 +9,12 @@
     </div>
 </template>
 <script>
-import headers from '@/common/header/header.vue';
+import headers from '@/components/common/header/header.vue';
 import banner from './banner/banner.vue';
 import selected from './selected/selected.vue';
 import guess from './guess/guess.vue';
 import recommend from './recommend/recommend.vue';
-import footers from '@/common/footer/footer';
+import footers from '@/components/common/footer/footer';
 export default {
     data(){
         return{
