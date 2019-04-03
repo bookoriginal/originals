6bcd1688d575628bc5e82f173c7fcf09b24dedb7
dmx123456 <13470003696@163.com>
2019-04-03 15:31:14 +0800

    'index_x'

--- original/src/App.vue
+++ original/src/App.vue
@@ -9,8 +9,8 @@
 <script>
 import login from './components/login' // 引入login.vue组件
 import index from './components/index/index'
-import headers from './common/header/header';
-import footers from './common/footer/footer';
+import headers from '@/components/common/header/header';
+import footers from '@/components/common/footer/footer';
 export default {
   name: 'App',
   components: {
