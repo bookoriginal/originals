9415687a91d79f1a2d9186134d50a0b0c3a71fd2
dmx123456 <13470003696@163.com>
2019-04-05 20:29:08 +0800

    'news_end'

---
+++ original/src/components/common/newsDetail/newsRelax/newsRelax.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="newsRelaxWrap">
+    <div class="newsRelaxBar">
+        <div class="relaxNews">相关新闻</div>
+        <div class="news">
+            <div class="newsLeft">
+                <p>2018十大考古新发现揭晓</p>
+                <p>2018十大考古新发现揭晓</p>
+                <p>2018十大考古新发现揭晓</p>
+                <p>2018十大考古新发现揭晓</p>
+            </div>
+            <div class="newsRight">
+                <p>2018十大考古新发现揭晓</p>
+                <p>2018十大考古新发现揭晓</p>
+                <p>2018十大考古新发现揭晓</p>
+                <p>2018十大考古新发现揭晓</p>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.newsRelaxWrap {
+  height: 443px;
+  width: 100%;
+  .newsRelaxBar{
+      height: 442px;
+      border-top:1px solid #eeeeee;
+      width: 1200px;
+      margin: 0 auto;
+     .relaxNews{
+         font-size: 26px;
+         margin-top: 110px;
+         margin-block-end: 28px;
+     }
+     .news{
+        display: flex;
+        font-size: 16px;
+        line-height: 40px;
+        div{
+            margin-right: 66px;
+        }
+     }
+  }
+}
+</style>
--- original/src/components/news/index.vue
+++ original/src/components/news/index.vue
@@ -1,23 +1,54 @@
 <template>
-  <div id= "news">
-    <nBanner></nBanner>
-    <aNews></aNews>
+  <div id="news">
+    <router-view v-show="this.newsDetailShow"></router-view>
+    <nBanner v-show="!this.newsDetailShow"></nBanner>
+    <aNews v-show="!this.newsDetailShow"></aNews>
+    <newsLists v-show="!this.newsDetailShow"></newsLists>
   </div>
 </template>
 
 <script>
-import nBanner from './newsBanner/newsBanner';
-import aNews from './activeNews/activeNews';
+import nBanner from "./newsBanner/newsBanner";
+import aNews from "./activeNews/activeNews";
+import newsLists from "./newsList/newsList";
 
 export default {
+  data() {
+    return {
+      newsDetailShow: false,
+      newsPath: "/" + this.$route.params.path
+    };
+  },
+  mounted() {
+    this.isNewsDetailShow();
+  },
+  methods: {
+    isNewsDetailShow() {
+      switch (this.newsPath) {
+        case "/news":
+          this.newsDetailShow = false;
+          break;
+        default:
+          this.newsDetailShow = true;
+          break;
+      }
+    }
+  },
   components: {
     nBanner,
     aNews,
+    newsLists
+  },
+  watch: {
+    $route(to, from) {
+      this.newsPath = to.path;
+      console.log(this.newsPath);
+      this.isNewsDetailShow();
+    }
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
-
 </style>
 
